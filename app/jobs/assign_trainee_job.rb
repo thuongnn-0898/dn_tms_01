eae6b6b07eef285be1bb9923f49e5a69dd7fe053
NguyenThuong <thuongnn1304@gmail.com>
2019-08-30 13:35:52 +0700

    delayjob_sendmail

--- app/jobs/assign_trainee_job.rb
+++ app/jobs/assign_trainee_job.rb
@@ -1,6 +1,6 @@
 class AssignTraineeJob < ApplicationJob
   queue_as :default
-  # action 1: assign, 2: remove
+  #action 1: assign, 2: remove
   def perform agr1, agr2, action
     if action == 1
       AssignMailer.send_mail(agr1, agr2)
